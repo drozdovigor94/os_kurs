19e067f0917e4012467e25902663eca668bf02bb
Igor Drozdov <drozdovigor94@gmail.com>
2018-12-18 16:48:46 +0300

    added some arg structure check and now working with config file as csv

--- my_module.py
+++ my_module.py
@@ -61,6 +61,8 @@ route_py_output:
 
 from ansible.module_utils.basic import AnsibleModule
 import os
+import csv
+
 
 def run_module():
 
@@ -68,10 +70,13 @@ def run_module():
     module_args = dict(
         route_py_path=dict(type='str', required=True),
         config_path=dict(type='str', required=True),
-        global_login=dict(type='str', required=True),
+        global_username=dict(type='str', required=True),
         global_password=dict(type='str', required=True),
         run_route_py=dict(type='bool', required=False, default=True),
-        routers=dict(type='list', elements='dict', required=False),
+        routers=dict(type='list', elements='dict', required=False,
+            options=dict(ip=dict(type='str', required=True),
+                         username=dict(type='str', required=False, default=''),
+                         password=dict(type='str', required=False, default='')))
     )   
 
     #define returned result format
@@ -91,39 +96,51 @@ def run_module():
         result['msg'] = 'No routers were specified'
         module.exit_json(**result)
 
-    # Try to open config file and get list of routers
+    fieldnames = ['#ip', 'username', 'password']
+
+    # Try to open config file or create if it doesn't exist
+    # and set up csv reader and writer
     try:
-        config_file = open(module.params['config_path'], 'r')
-        config_file.next()
-        existing_routers = [line.rstrip('\n') for line in config_file]
-        config_file.close()
+        config_file = open(module.params['config_path'], 'r+', newline='')
+        writer = csv.DictWriter(config_file, fieldnames=fieldnames)
+        reader = csv.DictReader(config_file)
+    except FileNotFoundError:
+        config_file = open(module.params['config_path'], 'w+', newline='')
+        writer = csv.DictWriter(config_file, fieldnames=fieldnames)
+        writer.writeheader()
+        reader = csv.DictReader(config_file)
 
-    except:
-        existing_routers = []
+    # Get list of existing routers and their ip's in particular
+    existing_routers = list(reader)
+    existing_routers_ips = [router['#ip'] for router in existing_routers]
 
-    # Format specified routers for route.py config
-    new_routers = [x['ip'] + ', ' + x['login'] + ', ' + x['password'] for x in module.params['routers']]
+    # change 'ip' key to '#ip' in new routers (so it follows required by route.py format)
+    new_routers = module.params['routers']
+    for router in new_routers:
+        router['#ip'] = router.pop('ip')
 
     # Check what routers are not in config file and add them
     add_count = 0
-    with open(module.params['config_path'], 'a+') as config_file:
-        for router in new_routers:
-            if router not in existing_routers:
-                result['changed'] = True
-                add_count = add_count + 1
-                config_file.write(router + '\n')
+    for router in new_routers:
+        if router['#ip'] not in existing_routers_ips:
+            result['changed'] = True
+            add_count = add_count + 1
+            writer.writerow(router)
+
+    #close config file
+    config_file.close()
 
     result['msg'] = "Existing routers: {}, new routers: {}, added {} routers".format(len(existing_routers), len(new_routers), add_count)
 
-    result['msg'] = [existing_routers, new_routers]
-
+    #result['msg'] = [existing_routers, new_routers]
+    
     # Run route.py if new routers were added
     if result['changed'] == True and module.params['run_route_py']:
         route_py_exists = os.path.isfile(module.params['route_py_path'])
         if route_py_exists:
             res = module.run_command(['python3', 
                 module.params['route_py_path'], 
-                module.params['global_login'], 
+                module.params['global_username'], 
                 module.params['global_password'], 
                 module.params['config_path']])
             if res[0] != 0:
